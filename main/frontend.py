251359c1d724732e4411b338e9c39835e2288118
jjean2006 <jacobjean999@gmail.com>
2025-09-22 10:58:49 +0530

    Configure web access through flask

--- main/frontend.py
+++ main/frontend.py
@@ -1,26 +1,52 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 CORS(app)
+print(app.root_path)
 
-path = "./web/"
 
-agent = request.headers.get('User-Agent')
-if ('iphone' or 'android' or 'blackberry') in agent.lower():
-    path += 'mobile/'
-else:
-    path += 'desktop/'
+#agent = request.headers.get('User-Agent')
+#if ('iphone' or 'android' or 'blackberry') in agent.lower():
+#    path += 'mobile/'
+#else:
+#    path += 'desktop/'
+
+def gen_path():
+        user_agent = request.headers.get("User-Agent", "").lower()
+        phones = ["android", "iphone", "ipad", "blackberry"]
+        for i in phones:
+            if i in user_agent:
+                path = "mobile/"
+                return path
+        else:
+            path = "desktop/"
+            return path
+
 
 @app.route('/')
-def index():
-    return render_template('./web/Homepage/index.html')
+def landing():
+    path = gen_path()
+    path += "landing/index.html"
+    return render_template(path)
 
 
 @app.route('/login')
-def index():
-    return render_template('./web/Login/index.html')
+def login():
+    path = "/common/login.html"
+    return render_template(path)
 
+@app.route('/staff/home')
+def staff_home():
+    path = gen_path()
+    path += "staff_homepage/index.html"
+    return render_template(path)
+
+@app.route('/student/home')
+def student_home():
+    path = gen_path()
+    path += "student_homepage/index.html"
+    return render_template(path)
 
 if __name__ == '__main__':
     app.run(debug=True)
